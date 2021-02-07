d5b96719a08451e576690743e041828fdb368335
nimowairimu <nimowairimu25@gmail.com>
2021-02-07 23:53:32 +0300

    Add style to the search user page .

--- src/app/search-user/search-user.component.ts
+++ src/app/search-user/search-user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search-user',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-user.component.css']
 })
 export class SearchUserComponent implements OnInit {
-
+  searchName:string;
+  @Output() searchOutput = new EventEmitter<any>()
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  search(){
+    this.searchOutput.emit(this.searchName);
+    this.searchName = "";
+  }
 }
